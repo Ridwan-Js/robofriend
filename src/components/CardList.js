5ac396e4df4d22a956f91eefbd6db7986abe449c
Ridwan Abidoye <ridwan.abidoye@smartflowtech.com>
2022-02-21 23:28:18 +0100

    deployment set-up

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error("oops! Something wrong in card list");
-  }
   return robots.map((user, i) => {
     return (
       <Card
